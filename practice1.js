e6f392f87cfb29c70182eec0db9797c62f77c9ec
JeonghoPark <pjhpjh0805@naver.com>
2023-07-05 19:35:02 +0900

    Initial Commit

---
+++ practice1.js
@@ -0,0 +1,32 @@
+const stack = [];
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+console.log(stack);
+
+console.log(stack.pop());
+
+
+stack.pop();
+stack.pop();
+
+console.log(stack);
+
+const user = {
+    name: "mike",
+    age: 30,
+};
+
+
+console.log(user);
+const user2 = user;
+user2.name = "Tom";
+
+console.log(user2);
+console.log(user);
+
+
+
+
