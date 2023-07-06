5327c4e991ca4243430ab27c1e6e4d2a14b51c78
JeonghoPark <pjhpjh0805@naver.com>
2023-07-06 22:01:51 +0900

    ES6 add

--- ES6.js
+++ ES6.js
@@ -32,5 +32,51 @@ const myFunc4 = (name, age) => {
 console.log(myFunc4('정호', 28));
 
 
+/**
+ * 기본 매게 변수
+ */
 
+// ES6 이전
+const myFunc5 = (name, age) => {
+    return `안녕 ${name} 너의 나이는 ${age}살이야?`;
+}
+console.log(myFunc5("정호")); // 정호 너의 나이는 undefined살이야?
+
+// ES6 기본 매게 변수 사용
+const myFunc6 = (name, age = 28) => {
+    return `안녕 ${name} 너의 나이는 ${age}살이야?`;
+}
+console.log(myFunc6("정호")); // 정호 너의 나이는 28살이야?
+
+
+/**
+ * 배열 및 객체 비구조화 할당
+ */
+
+// ES5 문법
+const member1 = {
+    name: '정호',
+    age: 28,
+};
+let memberNmae = member1.name;
+let memberAge = member1.age;
+
+// ES6 문법
+const member2 = {
+    name: '정호',
+    age: 28,
+}
+let {memberName : name, memberAge : age} = member2;
+console.log(memberNmae, memberAge); // 정호 28
+
+
+/**
+ * Promise
+ */
+const myPromise = () => {
+    return new Promise((resolve, reject) => {
+        resolve('안녕하세요 Promise가 성공적으로 실행되었습니다.');
+    });
+};
+console.log(myPromise());
 
