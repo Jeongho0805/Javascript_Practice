dd55f9fc4c086f5f7b4c96b65dcc3ca80f3dc89b
JeonghoPark <pjhpjh0805@naver.com>
2023-07-09 16:58:09 +0900

    commit

--- BaseBallGame/src/Game.js
+++ BaseBallGame/src/Game.js
@@ -2,7 +2,7 @@ const { Console } = require("@woowacourse/mission-utils");
 const Computer = require("./Computer");
 const User = require("./User");
 
-class Gmae {
+class Game {
     constructor() {
         this.computer = new Computer();
         this.user = new User();
@@ -17,6 +17,12 @@ class Gmae {
         this.user.setNumber();
         const result = this.countResult();
         this.printResult(result);
+        if (this.isEnd(result)) {
+            return askUser();
+        }
+        this.play();
+        
+        
     }
 
 
@@ -26,13 +32,15 @@ class Gmae {
 
         const answerNumber = this.computer.getNumber;
 
-        this.user.getNumber.forEach((value, index) => {
+        console.log(this.user.getNumber())
+
+        this.user.getNumber().forEach((value, index) => {
             if (value === answerNumber[index]) {
                 strike++;
             } else if (answerNumber.include(value)) {
                 ball++;
             }
-        })
+        });
         return { ball, strike};
     }
 
@@ -43,7 +51,37 @@ class Gmae {
         const ballText = ball? `${ball}볼` : "";
         const strikeText = strike? `${strike}스트라이크` : "";
         Console.print(`${ballText} ${strikeText}`);
+    }
 
+    isEnd({ strike }) {
+        if (strike === 3) {
+            Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+        return false;
+    }
+
+    askUser() {
+        Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        const input = +Console.readLine();
+        if (input === 1) {
+            return this.replay();
+        }
+        else if (input === 2) {
+            return this.exit();
+        }
+        else {
+            throw new Error("잘못된 입력입니다. 게임이 종료됩니다.")
+        }
+    }
+
+    replay() {
+        this.start();
+    }
+
+    exit() {
+        Console.close();
     }
 }  
 
